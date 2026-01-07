7b60f3eb09f30c20a4bfbd243a53dd622dc12747
igor rocha <iogorrochamiranda2@gmail.com>
2026-01-06 23:05:51 -0300

    Solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,16 @@
-export const CommentList = () => <>Put the list here</>;
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+import './CommentList.scss';
+
+export const CommentList = ({ post, comments }) => {
+  const postComments = post
+    ? comments.filter(comment => comment.postId === post.id)
+    : comments;
+
+  return (
+    <div>
+      {postComments.map(comment => (
+        <CommentInfo key={comment.id} comment={comment} />
+      ))}
+    </div>
+  );
+};
