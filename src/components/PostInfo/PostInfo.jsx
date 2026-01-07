6b7ec257b87b5227c7032c64c52ce0de68fee720
igor rocha <iogorrochamiranda2@gmail.com>
2026-01-06 23:11:27 -0300

    Remuve key in PostInfo

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -4,32 +4,30 @@ import { UserInfo } from '../UserInfo/UserInfo';
 
 export const PostInfo = ({ post }) => (
   <div>
-    <div key={post.id}>
-      <div className="PostInfo__header">
-        <div className="PostInfo__title">
-          <h3>{post.title}</h3>
+    <div className="PostInfo__header">
+      <div className="PostInfo__title">
+        <h3>{post.title}</h3>
+      </div>
+
+      <p>
+        {' Posted by  '}
+        <UserInfo user={post.user} />
+      </p>
+    </div>
+
+    <div className="PostInfo__body">
+      <p>{post.body}</p>
+    </div>
+
+    <div className="CommentList">
+      {post.comments && post.comments.length > 0 ? (
+        <CommentList comments={post.comments} />
+      ) : (
+        <div>
+          <hr />
+          <b data-cy="NoCommentsMessage">No comments yet</b>
         </div>
-
-        <p>
-          {' Posted by  '}
-          <UserInfo user={post.user} />
-        </p>
-      </div>
-
-      <div className="PostInfo__body">
-        <p>{post.body}</p>
-      </div>
-
-      <div className="CommentList">
-        {post.comments && post.comments.length > 0 ? (
-          <CommentList comments={post.comments} />
-        ) : (
-          <div>
-            <hr />
-            <b data-cy="NoCommentsMessage">No comments yet</b>
-          </div>
-        )}
-      </div>
+      )}
     </div>
   </div>
 );
